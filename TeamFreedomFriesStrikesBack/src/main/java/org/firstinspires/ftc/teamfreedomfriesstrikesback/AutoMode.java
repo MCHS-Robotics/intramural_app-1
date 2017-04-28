2718b693d9a5a8a33deeed152000421e5ef4eb4e
firemew <kalebfire@cox.net>
2017-04-27 18:34:39 -0700

    not mine -bryan had to commit to be able to pull

---
+++ TeamFreedomFriesStrikesBack/src/main/java/org/firstinspires/ftc/teamfreedomfriesstrikesback/AutoMode.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamfreedomfriesstrikesback;
+
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.hardware.GyroSensor;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * OpMode for testing correct connection and direction of drive motors.
+ * Spins each motor starting with the FL and going clockwise.
+ */
+
+@Autonomous(name = "auto", group = "templates")
+//@Disabled
+
+public class AutoMode extends LinearOpMode {
+
+    //private ElapsedTime runtime = new ElapsedTime();
+
+
+    DcMotor L, R;
+
+    public ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        L = hardwareMap.dcMotor.get("left");
+        R = hardwareMap.dcMotor.get("right");
+        L.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        R.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        L.setDirection(DcMotor.Direction.REVERSE);
+
+
+        idle();
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+        runtime.reset();
+        /*Insert Code Here*/
+        L.setPower(.2);
+        R.setPower(.2);
+        double time = runtime.seconds();
+        while(opModeIsActive() && runtime.seconds()-time > 3);
+        L.setPower(0);
+        R.setPower(0);
+        while(opModeIsActive() && runtime.seconds()-time > 1);
+        L.setPower(-.2);
+        R.setPower(-.2);
+        while(opModeIsActive() && runtime.seconds()-time > 3.5);
+        L.setPower(0);
+        R.setPower(0);
+        /*Insert Code Here*/
+        telemetry.addData("Status", "Complete");
+        telemetry.update();
+
+
+    }
+
+
+}
